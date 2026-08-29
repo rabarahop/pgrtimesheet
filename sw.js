cedc00a5fb972458f2feacd130c804817315b34f
rabarahop <rabarahop@gmail.com>
2026-08-29 17:19:50 -0600

    Implement cache cleanup in service worker
    Add cache management during service worker activation.

--- sw.js
+++ sw.js
@@ -25,3 +25,19 @@ self.addEventListener('fetch', event => {
       })
   );
 });
+
+// Actualizar el Service Worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
